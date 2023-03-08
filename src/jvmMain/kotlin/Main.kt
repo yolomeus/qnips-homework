029b116df0811aebc6f99e4b8652a089d537260a
fabian <fab.beringer@gmail.com>
2023-03-08 18:45:04 +0100

    update basic structure of main

--- src/jvmMain/kotlin/Main.kt
+++ src/jvmMain/kotlin/Main.kt
@@ -1,36 +1,46 @@
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.remember
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Window
+import androidx.compose.ui.window.WindowState
 import androidx.compose.ui.window.application
 import kotlinx.coroutines.Dispatchers
 import model.Api
 import model.RemoteDataSource
-import viewmodel.AllergensViewModel
+import viewmodel.RowViewModel
 
 
-fun main() = application {
+@Composable
+fun App(dataSource: RemoteDataSource) {
+    val rowVm = RowViewModel(dataSource)
 
+    val rows by remember { rowVm.rows }.collectAsState(Dispatchers.IO)
+
+    MaterialTheme {
+        Button(onClick = {
+            dataSource.updateData()
+        }) {
+            Text("load")
+        }
+        Text(rows.toString())
+    }
+}
+
+fun main() {
     val source = RemoteDataSource(Api.service)
-    val allergensVm = AllergensViewModel(source)
 
-    Window(
-        onCloseRequest = ::exitApplication,
-        title = "Menu"
-    ) {
-
-        val allergens by remember { allergensVm.allergens }.collectAsState(Dispatchers.IO)
-
-        MaterialTheme {
-            Button(onClick = {
-                allergensVm.updateState()
-            }) {
-                Text("load")
-            }
-            Text(allergens.toString())
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            title = "Menu",
+            state = WindowState(width = 1200.dp, height = 800.dp)
+        ) {
+            App(source)
         }
     }
 }
